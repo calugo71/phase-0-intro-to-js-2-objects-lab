1a767cbd5f98080e0d1aa2565208025ef5b14250
Carlos Lugo <calugo71@gmail.com>
2022-07-20 04:41:54 -0400

    commit tree 9f3f94b5d96cc94c6e924598945f5ce8f92d3ff4

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution in this file!
+const employee= {
+    name:"Sam", steetAddress:"11 Broadway" 
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return console.log(...employee)
+}
